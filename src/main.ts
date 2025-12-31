ead49ac45477fe81614c02ea66567773c40d3783
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-12-31 20:10:50 +0200

    optional and default value

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,10 @@
 import { z } from "zod";
 
-// Creating Zod Schema
 const userSchema = z.object({
   name: z.string(),
-  age: z.number(),
-  email: z.email(),
+  age: z.number().optional(),
+  country: z.string().default("Unknown"),
 });
 
-// The data
-const user1 = {
-  name: "Abdallah",
-  age: 21,
-  email: "abdallah@example.com",
-};
-const user2 = {
-  name: 1,
-};
-
-// Validate the data using the schema
-
-const validUser1 = userSchema.parse(user1);
-console.log(validUser1);
-
-const validUser2 = userSchema.parse(user2);
-console.log(validUser2);
-
-const validUser2Error = userSchema.safeParse(user2);
-console.log(validUser2Error.error?.issues);
+const user = { name: "Abdo", age: 22, country: "Estonia" };
+console.log(userSchema.parse(user));
